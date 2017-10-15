0b77f1737f0072f6bfdb797fbf404c5b608cc380
sleyzerzon <senya22@yahoo.com>
2017-10-15 13:55:29 -0400

    eureka client

--- service-registry-and-discovery-with-eureka/eureka-client/src/main/java/com/excelsiorsoft/spring/cloud/EurekaClientApplication.java
+++ service-registry-and-discovery-with-eureka/eureka-client/src/main/java/com/excelsiorsoft/spring/cloud/EurekaClientApplication.java
@@ -2,8 +2,10 @@ package com.excelsiorsoft.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EurekaClientApplication {
 
 	public static void main(String[] args) {
