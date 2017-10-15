526a44978f8639efb9ffefd74610675814465e60
sleyzerzon <senya22@yahoo.com>
2017-10-15 18:54:10 -0400

    docs

--- service-registry-and-discovery-with-eureka/eureka-client2/src/main/java/com/excelsiorsoft/spring/cloud/EurekaClientApplication.java
+++ service-registry-and-discovery-with-eureka/eureka-client2/src/main/java/com/excelsiorsoft/spring/cloud/EurekaClientApplication.java
@@ -1,8 +1,11 @@
 package com.excelsiorsoft.spring.cloud;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +19,18 @@ import com.netflix.discovery.EurekaClient;
 public class EurekaClientApplication {
 	
 	@Autowired
+	private DiscoveryClient discoveryClient;
+	
+	@Autowired
 	private EurekaClient client;
 	
 	@RequestMapping("/serviceInfo")
 	public String serviceInfo() {
+		
+		discoveryClient.getInstances("firstService").forEach((ServiceInstance s) -> {
+			System.out.println(ToStringBuilder.reflectionToString(s));
+		});
+		
 		InstanceInfo info = client.getNextServerFromEureka("firstService", false);
 		return info.getHomePageUrl();
 	}
