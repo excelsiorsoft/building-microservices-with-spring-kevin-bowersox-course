1e61e2897fef96c9be75d37d12952b563ab62191
sleyzerzon <senya22@yahoo.com>
2017-10-16 15:10:24 -0400

    oauth

---
+++ token-based-security-with-oauth/oauth-server/src/main/java/com/excelsiorsoft/spring/cloud/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.excelsiorsoft.spring.cloud;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManager();		
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user1").password("password1").roles("USER");
+	}
+	
+}
