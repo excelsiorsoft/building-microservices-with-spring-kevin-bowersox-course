fe4b808f1023161f6539e85017e00e80523ba167
sleyzerzon <senya22@yahoo.com>
2017-10-16 15:29:43 -0400

    oauth

--- token-based-security-with-oauth/oauth-server/src/main/java/com/excelsiorsoft/spring/cloud/OauthServerApplication.java
+++ token-based-security-with-oauth/oauth-server/src/main/java/com/excelsiorsoft/spring/cloud/OauthServerApplication.java
@@ -3,11 +3,18 @@ package com.excelsiorsoft.spring.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableAuthorizationServer
 public class OauthServerApplication {
 
+	
+	@RequestMapping("/resource")
+	public String resourceEndpoint() {
+		return "This resource is protected by the resource server.";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(OauthServerApplication.class, args);
 	}
