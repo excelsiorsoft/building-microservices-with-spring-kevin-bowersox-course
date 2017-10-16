4a7cb5156e6c901e3d0589d0e35e4d3e7d768d0c
sleyzerzon <senya22@yahoo.com>
2017-10-15 21:45:11 -0400

    hystrix

--- curcuit-breaker-fault-tolerance-with-hystrix/hystrix/src/main/java/com/excelsiorsoft/spring/cloud/HystrixApplication.java
+++ curcuit-breaker-fault-tolerance-with-hystrix/hystrix/src/main/java/com/excelsiorsoft/spring/cloud/HystrixApplication.java
@@ -9,10 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @SpringBootApplication
 @RestController
@@ -26,13 +28,17 @@ public class HystrixApplication {
 	
 	@Autowired private RestTemplate restTemplate;
 	
+	@SuppressWarnings("unchecked")
 	@RequestMapping("/startClient")
-	@HystrixCommand(fallbackMethod="failover")
-	public List<String> startClient(){
+	@HystrixCommand(fallbackMethod="failover", commandProperties= {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500")
+	})
+	public List<String> startClient(@RequestParam long time) throws InterruptedException{
+		Thread.sleep(time);
 		return restTemplate.getForObject("http://localhost:8899/service", List.class);
 	}
 	
-	public List<String> failover(){
+	public List<String> failover(long time){
 		return Arrays.asList("Default1", "Default2");
 	}
 	
